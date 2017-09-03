e7c4d4eef346786db7633054ee90852298fed5cf
黐麟 <tirin.script.modder@gmail.com>
2017-09-03 22:18:13 +0900

    Update GitHub_Loader_Target.js

--- GitHub_Loader_Target.js
+++ GitHub_Loader_Target.js
@@ -3,6 +3,8 @@ function main(){
 }
 (function(){
 	main();
+	ModPE.setItem(1000,'diamond',0,'偽物のダイアモンド',64);
+	Player.addItemCreativeInv(1000,1,0);
 })();
 function newLevel(){
 	clientMessage('読み込めてるよ');
